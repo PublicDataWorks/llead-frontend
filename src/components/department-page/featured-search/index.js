22ade33afafcb9dc1d674b00a27254dad497a155
East Agile <open-source@eastagile.com>
2022-02-15 16:24:00 +0700

    [b] 829 - Missing space after search query on search results
    [#181069829](https://www.pivotaltracker.com/story/show/181069829)

--- src/components/department-page/featured-search/index.js
+++ src/components/department-page/featured-search/index.js
@@ -94,7 +94,7 @@ const FeaturedSearch = (props) => {
         <div className='search-result'>
           <div className='search-result-count'>{`${count} Search results for "${searchQuery}" `}</div>
           <div className='search-result-department'>
-            {isMobile ? departmentName : ` | ${departmentName}`}
+            {isMobile ? departmentName : `| ${departmentName}`}
           </div>
         </div>
       )}
